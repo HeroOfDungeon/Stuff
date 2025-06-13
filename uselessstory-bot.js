9eb255114105634dcb372190d9ad09b1c8d574c6
HeroOfDungeon <170803718+HeroOfDungeon@users.noreply.github.com>
2025-06-13 22:38:51 +0200

    Update uselessstory-bot.js

--- uselessstory-bot.js
+++ uselessstory-bot.js
@@ -1,9 +1,9 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
-const channel_id = "1243869409265844277"
+const channel_id = "1356162138481364992"
 const correct_content = ["#uselessstory", `<#${channel_id}>`]
-const message_delete_delay = 60000 // Messured in ms
+const message_delete_delay = 5 // Messured in s
 
 const client = new Client({
     intents: [
@@ -23,12 +23,13 @@ client.on('messageCreate', async message => {
 
     if (!correct_content.some(word => message.content.toLowerCase().includes(word))) { // Check if none of the correct words are in the message
         const msg = await message.reply({ // Reply with message
-            content: `Remember to add \`#uselessstory\` to your message!\n-# This message deletes itself in 5 seconds`
+            content: `Remember to add \`#uselessstory\` to your message!\n-# Your message gets deleted <t:${Math.floor(Date.now() / 1000) + message_delete_delay + 2}:R>`
         });
 
-        setTimeout(() => { // Delete after 5000ms
+        setTimeout(() => { // Delete messages after set time
             msg.delete().catch(console.error)
-        }, message_delete_delay);
+            message.delete().catch(console.error)
+        }, (message_delete_delay * 1000));
     }
 });
 
